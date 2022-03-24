516c2ecbcbcf71af597155c8df964c4d02647af5
vsgn <vinicius.goncalves@gft.com>
2022-03-24 09:22:44 -0300

    feat: Login page and App.js changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import FormLogin from "../components/Forms/FormLogin";
+import logo from '../assets/img/login/icone-de-dinheiro.png';
+import '../assets/styles/login/login.css';
+import { Container, ContainerLogin, Title, Logo } from "../assets/styles/login/login";
 
 const Login = () => {
   return (
-    <div />
+    <Container>
+      <ContainerLogin>
+        <Logo src={logo} />
+        <Title>LOGIN</Title>
+        <FormLogin />
+      </ContainerLogin>
+    </Container>
   )
 }
 
