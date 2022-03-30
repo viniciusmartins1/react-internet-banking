4d2766d1609723384169d70967ecb6d02386a53b
vsgn <vinicius.goncalves@gft.com>
2022-03-30 11:27:48 -0300

    feat: import components to the page

--- src/pages/CoOwners.jsx
+++ src/pages/CoOwners.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import TableCoOwners from "../components/Co-owners/TableCoOwners";
+import Header from "../components/Home/Header";
+import LateralMenu from "../components/Home/LateralMenu";
 
 const CoOwners = () => {
   return (
-    <div />
+    <>
+      <Header />
+      <LateralMenu />
+      <TableCoOwners />
+    </>
   )
 }
 
