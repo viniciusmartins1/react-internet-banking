e45fb97d596cc714886d766ae7fb26eae4af39fa
vsgn <vinicius.goncalves@gft.com>
2022-03-31 00:03:24 -0300

    feat: import the components to the page

--- src/pages/NewCoOwner.jsx
+++ src/pages/NewCoOwner.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import FormNewCoOwner from "../components/Forms/FormNewCoOwner";
+import Header from "../components/Home/Header";
+import LateralMenu from "../components/Home/LateralMenu";
 
 const NewCoOwner = () => {
   return (
-    <div />
+    <>
+      <Header />
+      <LateralMenu />
+      <FormNewCoOwner />
+    </>
   )
 }
 
