2b7d3a5b616b2ed6b841c7fd1173c3cf59feba5f
vsgn <vinicius.goncalves@gft.com>
2022-03-31 00:01:58 -0300

    feat: validations for the co-owners form

--- src/utils/validateChar.js
+++ src/utils/validateChar.js
@@ -8,7 +8,32 @@ export const CheckLettersAndNumbers = (string) => {
   }
 }
 
-export const CheckJustNumbers = (string) =>  {
+export const CheckJustLetters = (string) => {
+
+  const newString = string.replace(/\s+/g, '');
+  const pattern = '[a-zA-Z]';
+
+  for (var char of newString) {
+    if (!char.match(pattern)) {
+      return true;
+    }
+  }
+}
+
+export const CheckCPF = (string) => {
+
+  const pattern = '[0-9]';
+
+  for (var char of string) { 
+    if (!char.match(pattern)) {
+      return true;
+    }
+  }
+}
+
+
+export const CheckJustNumbers = (string) => {
+
   const pattern = '[0-9]';
 
   for (var char of string) {
