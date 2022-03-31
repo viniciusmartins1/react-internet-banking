4ac379ac7cad2875f92e9036c2a71fcadc4313b1
vsgn <vinicius.goncalves@gft.com>
2022-03-30 23:59:59 -0300

    feat: mocks with co-owners list

---
+++ src/mocks/listCoOwners.js
@@ -0,0 +1,41 @@
+export const listCoOWners = (
+  [
+    {
+      name: 'Ana Laura da Cruz',
+      cpf: '435.315.420-60',
+      birthDate: '24/02/2002',
+      phoneNumber: '(63) 92772-6284',
+    },
+    {
+      name: 'Carolina Sabrina ',
+      cpf: '533.192.420-40',
+      birthDate: '01/02/2002',
+      phoneNumber: '(84) 93258-2439',
+    },
+    {
+      name: 'Matheus Gael ',
+      cpf: '533.192.420-40',
+      birthDate: '02/03/1999',
+      phoneNumber: '(67) 93286-9318',
+    },
+    {
+      name: 'Murilo Bryan ',
+      cpf: '745.822.410-00',
+      birthDate: '06/01/1999',
+      phoneNumber: '(96) 92864-3132',
+    },
+    {
+      name: 'Cláudia Tatiane ',
+      cpf: '353.279.520-53',
+      birthDate: '17/01/1992',
+      phoneNumber: '(41) 39935-8317',
+    },
+    {
+      name: 'Jéssica Almada',
+      cpf: '272.942.700-74',
+      birthDate: '12/01/1992',
+      phoneNumber: '(69) 39295-6673',
+    },
+
+  ]
+)
