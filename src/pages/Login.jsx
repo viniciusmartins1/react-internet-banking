fa34e6f8b9370f960e1dce6fe47806081da76979
vsgn <vinicius.goncalves@gft.com>
2022-04-01 01:06:23 -0300

    style: logo style change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,14 @@ import FormLogin from "../components/Forms/FormLogin";
 import logo from '../assets/img/logo.png';
 import '../assets/styles/login/login.css';
 import { Container, ContainerLogin, Title, Logo } from "../assets/styles/login/login";
+import { TitleLogoColor } from "../assets/styles/home/home";
 
 const Login = () => {
   return (
     <Container>
       <ContainerLogin>
-        <Logo src={logo} />
+        {/* <Logo src={logo} /> */}
+        <TitleLogoColor>Internet Banking</TitleLogoColor>
         <Title>LOGIN</Title>
         <FormLogin />
       </ContainerLogin>
