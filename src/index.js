2bdd3c82b8e8f83ec3c91390a30565282b823f09
vsgn <vinicius.goncalves@gft.com>
2022-04-03 13:05:46 -0300

    feat: implementation of redux persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import store from "./redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor} from "./redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,22 @@ import createSaga from 'redux-saga';
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import { sagas } from './services';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
 const sagaMiddleware = createSaga(); 
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
-export default store;  
+export { store, persistor};  
